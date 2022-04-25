b401a7cbd611c153fd6f67c5e4d1b6dc74ce8ed4
Marcos Torres <marcmam2@gmail.com>
2022-04-25 09:10:39 +0000

    refactoring exercises 01, 02

--- src/lista_de_exercícios01/Exercise01.java
+++ src/lista_de_exercícios01/Exercise01.java
@@ -6,11 +6,13 @@ public class Exercise01 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    System.out.print("Valor de uma temperatura em graus Celsius para ser convertido: ");
+
     float celsius = scanner.nextFloat();
 
-    System.out.println("Kelvin: " + (celsius + 273.15));
-    System.out.println("Fahrenheit: " + (celsius * 1.8 + 32));
-    System.out.println("Rankine: " + (celsius * 1.8 + 32 + 459.67));
-    System.out.println("Réaumur: " + celsius * 0.8);    
+    System.out.println("em Kelvin: " + (celsius + 273.15));
+    System.out.println("em graus Fahrenheit: " + (celsius * 1.8 + 32));
+    System.out.println("em Rankine: " + (celsius * 1.8 + 32 + 459.67));
+    System.out.println("em graus Réaumur: " + celsius * 0.8);    
   }
 }
