09d20a73968bc808dc202a2662464cf334b10b36
Marcos Torres <marcmam2@gmail.com>
2022-04-25 04:02:48 +0000

    setting up Replit

--- src/lista_de_exercícios01/Exercise01.java
+++ src/lista_de_exercícios01/Exercise01.java
@@ -2,15 +2,15 @@ package lista_de_exercícios01;
 
 import java.util.Scanner;
 
-class Exercise01 {
+public class Exercise01 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     float celsius = scanner.nextFloat();
 
     System.out.println("Kelvin: " + (celsius + 273.15));
-    System.out.println("Fahrenheit: " + celsius * 1.8 + 32);
-    System.out.println("Rankine: " + celsius * 1.8 + 32 + 459.67);
+    System.out.println("Fahrenheit: " + (celsius * 1.8 + 32));
+    System.out.println("Rankine: " + (celsius * 1.8 + 32 + 459.67));
     System.out.println("Réaumur: " + celsius * 0.8);    
   }
 }
