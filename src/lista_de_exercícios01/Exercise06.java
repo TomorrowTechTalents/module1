3dbd8258665266d9ae331e14972e757e22ec5dc5
marcmam2 <marcmam2@gmail.com>
2022-04-26 06:35:10 -0300

    exercise 5.1 + minor changes

--- src/lista_de_exercícios01/Exercise06.java
+++ src/lista_de_exercícios01/Exercise06.java
@@ -8,14 +8,19 @@ public class Exercise06 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    System.out.print("valor em reais: R$ ");
     BigDecimal valueInBRL = scanner.nextBigDecimal();
 
     BigDecimal USDRate = new BigDecimal("4.70");
     BigDecimal EURRate = new BigDecimal("5.11");
     BigDecimal GBPRate = new BigDecimal("6.12");
 
-    System.out.println("valor em dólares americanos: $ " + valueInBRL.divide(USDRate, 2, RoundingMode.HALF_EVEN);
-    System.out.println("valor em euros: € " + valueInBRL.divide(EURRate, 2, RoundingMode.HALF_EVEN));
-    System.out.println("valor em libras esterlinas: £ " + valueInBRL.divide(GBPRate, 2, RoundingMode.HALF_EVEN));
+    System.out.println();
+    System.out.println("valor em dólares americanos: $ " +
+                       valueInBRL.divide(USDRate, 2, RoundingMode.HALF_EVEN));
+    System.out.println("valor em euros: € " +
+                       valueInBRL.divide(EURRate, 2, RoundingMode.HALF_EVEN));
+    System.out.println("valor em libras esterlinas: £ " +
+                       valueInBRL.divide(GBPRate, 2, RoundingMode.HALF_EVEN));
   }
 }
--- src/lista_de_exercícios05/Exercise01.java
+++ src/lista_de_exercícios05/Exercise01.java
@@ -1,11 +1,31 @@
 package lista_de_exercícios05;
 
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Exercise01 {
     public static void main(String[] args) {
+        final BigDecimal DAYS_PER_YEAR = new BigDecimal("365.242");
+        final BigDecimal CIGARETTES_PER_PACK = new BigDecimal("20");
+
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("período de tempo fumando, em anos: ");
+        BigDecimal timeSmokingInYears = scanner.nextBigDecimal();
 
+        System.out.print("quantidade média aproximada de cigarros consumidos por dia: ");
+        BigDecimal numberOfCigarettesPerDay = scanner.nextBigDecimal();
+
+        System.out.print("preço de 1 carteira de 20 cigarros: ");
+        BigDecimal cigarettesPackPrice = scanner.nextBigDecimal();
+
+        BigDecimal timeSmokingInDays = timeSmokingInYears.multiply(DAYS_PER_YEAR);
+
+        BigDecimal cigaretteUnitPrice = cigarettesPackPrice.
+                                        divide(CIGARETTES_PER_PACK, 2, RoundingMode.HALF_EVEN);
+
+        System.out.println("\ndinheiro gasto, aproximadamente: " + cigaretteUnitPrice.
+                           multiply(numberOfCigarettesPerDay).multiply(timeSmokingInDays));
     }
 }
