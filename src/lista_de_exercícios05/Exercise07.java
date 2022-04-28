dedce34c7d896bb628aab86dbe0c89efab085c45
marcmam2 <marcmam2@gmail.com>
2022-04-28 08:54:15 -0300

    exercise 5.7

--- src/lista_de_exercícios05/Exercise07.java
+++ src/lista_de_exercícios05/Exercise07.java
@@ -1,6 +1,7 @@
 package lista_de_exercícios05;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,21 +9,57 @@ public class Exercise07 {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
 
+        final byte NUMBER_OF_TRANSACTIONS = 4;
+        final BigDecimal NUMBER_OF_INSTALLMENTS = new BigDecimal("3");
+
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 15; i++) {                   //constant
+        boolean inInstallmentsSaleRegistered = false;
+        BigDecimal sumOfValuesOfSalesInFull = BigDecimal.ZERO;
+        BigDecimal sumOfValuesOfSalesInInstallments = BigDecimal.ZERO;
+
+        BigDecimal[] installmentsValues = new BigDecimal[NUMBER_OF_TRANSACTIONS];
+
+        for (int i = 0; i < NUMBER_OF_TRANSACTIONS; i++) {
+            System.out.print("código da transação: ");
             char transactionCode = scanner.next().charAt(0);
-            BigDecimal originalValue = scanner.nextBigDecimal();
 
-            BigDecimal sumOfValuesOfSalesInFull = BigDecimal.ZERO;
-            BigDecimal sumOfValuesOfSalesInInstallments = BigDecimal.ZERO;
+            if (transactionCode == 'V') {            // enumerator
+                System.out.print("valor da transação: R$ ");
+                BigDecimal value = scanner.nextBigDecimal();
 
-            if (transactionCode == 'V') {            // constant
-               // sumOfSalesInFullValues += originalValue;
+                sumOfValuesOfSalesInFull = sumOfValuesOfSalesInFull.add(value);
             } else if (transactionCode == 'P') {
-                //sumOfSalesInInstallmentsValues =
+                inInstallmentsSaleRegistered = true;
+
+                System.out.print("valor original da transação: R$ ");
+
+                BigDecimal originalValue = scanner.nextBigDecimal();
+
+                installmentsValues[i] = originalValue.divide(NUMBER_OF_INSTALLMENTS, 2, RoundingMode.DOWN);
+
+                sumOfValuesOfSalesInInstallments = sumOfValuesOfSalesInInstallments.
+                                                   add(installmentsValues[i].multiply(NUMBER_OF_INSTALLMENTS));
             } else {
                 throw new IllegalArgumentException("apenas códigos \"V\" e \"P\" são válidos");
+            }
+
+            System.out.println("==========");
+        }
+
+        BigDecimal totalSalesValue = sumOfValuesOfSalesInFull.add(sumOfValuesOfSalesInInstallments);
+
+        System.out.println("valor total das compras à vista: " + sumOfValuesOfSalesInFull);
+        System.out.println("valor total das compras a prazo: " + sumOfValuesOfSalesInInstallments);
+        System.out.println("valor total das compras efetuadas: " + totalSalesValue);
+
+        if (inInstallmentsSaleRegistered) {
+            System.out.println("valor(es) da(s) prestação(ões) da(s) compra(s) a prazo: ");
+
+            for (int i = 0; i < NUMBER_OF_TRANSACTIONS; i++) {
+                if (installmentsValues[i] != null) {
+                    System.out.println("compra " + i + ": " + installmentsValues[i]);
+                }
             }
         }
     }
